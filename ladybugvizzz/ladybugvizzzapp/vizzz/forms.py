4a5fe3de792f8cd898a6eb06eb9f5584d24989f1
Chris Mackey <chris@mackeyarchitecture.com>
2017-10-22 02:49:50 -0400

    Updated Form

--- ladybugvizzz/ladybugvizzzapp/vizzz/forms.py
+++ ladybugvizzz/ladybugvizzzapp/vizzz/forms.py
@@ -1,5 +1,17 @@
 from django import forms
+from django.db import models
+
+# Create your models here.
+marcas = (
+        ('chevrolet', 'Chevrolet'),
+        ('mazda', 'Mazda'),
+        ('nissan', 'Nissan'),
+        ('toyota', 'Toyota'),
+        ('mitsubishi', 'Mitsubishi'),)
+
+class Marca(models.Model):
+    marca = models.CharField(max_length=25,choices=marcas)
 
 class CityForm(forms.Form):
     city_name = forms.CharField(label='City Name', max_length=100)
-    #data_type = forms.ChoiceField(label='Data Type')
+    data_type = forms.ChoiceField(label='Data Type', choices=marcas)
